e1b75432f1e1cbf67155e65a10a25e2c639e99fc
kabra4 <kabra0413@gmail.com>
2021-02-04 21:02:52 +0500

    Draws Cube

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -30,24 +30,39 @@ namespace Rubicks_Cube
         private void InitializeComponent()
         {
             this.panel1 = new System.Windows.Forms.Panel();
+            this.button1 = new System.Windows.Forms.Button();
+            this.panel1.SuspendLayout();
             this.SuspendLayout();
             // 
             // panel1
             // 
+            this.panel1.Controls.Add(this.button1);
             this.panel1.Location = new System.Drawing.Point(12, 12);
             this.panel1.Name = "panel1";
             this.panel1.Size = new System.Drawing.Size(600, 600);
             this.panel1.TabIndex = 0;
             this.panel1.Paint += new System.Windows.Forms.PaintEventHandler(this.panel1_Paint);
             // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button1.Location = new System.Drawing.Point(49, 192);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(50, 50);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "<";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(621, 620);
+            this.ClientSize = new System.Drawing.Size(857, 620);
             this.Controls.Add(this.panel1);
             this.Name = "Form1";
             this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.panel1.ResumeLayout(false);
             this.ResumeLayout(false);
 
         }
@@ -55,6 +70,7 @@ namespace Rubicks_Cube
         #endregion
 
         private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Button button1;
     }
 }
 
--- Form1.cs
+++ Form1.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.Drawing.Drawing2D;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -17,50 +18,100 @@ namespace Rubicks_Cube
             InitializeComponent();
         }
 
-        private enum Cube
-        {
-            White,
-            Red,
-            Yellow,
-            Orange,
-            Green,
-            Blue
-        }
-
-        private Cube[,] sides = new Cube[6, 9];
+        private Color[,] cells = new Color[6, 9];
+        private Color[] colors = {
+            Color.White,
+            Color.Red,
+            Color.Yellow,
+            Color.Orange,
+            Color.Blue,
+            Color.Green
+        };
 
         private void NewCube()
         {
-            var temp = 0;
-            foreach (Cube color in Enum.GetValues(typeof(Cube)))
+            for (int s = 0; s < 6; s++)
             {
                 for (int i = 0; i < 9; i++)
                 {
-                    sides[temp, i] = color;
-                    temp++;
+                    cells[s, i] = colors[s];
+                    
                 }
             }
         }
 
         private void panel1_Paint(object sender, PaintEventArgs e)
         {
-            var c = 200/Math.Cos(0.523599);
-            var g = e.Graphics;
+            NewCube();
+            PaintCube(e);
+            
 
             // Painting lines
-            for (int i = 0; i < 4; i++) 
-            {
-                g.DrawLine(Pens.Black, 100, (float)(163 + c / 3 * i), 300, (float)(281 + c / 3 * i));
-                g.DrawLine(Pens.Black, 300, (float)(281 + c / 3 * i), 500, (float)(165 + c / 3 * i));
-                g.DrawLine(Pens.Black, 300 + 200 / 3 * i, (float)(281 - c / 6 * i), 300 + 200 / 3 * i, (float)(281 - c / 6 * i + c));
-                g.DrawLine(Pens.Black, 300 - 200 / 3 * i, (float)(281 - c / 6 * i), 300 - 200 / 3 * i, (float)(281 - c / 6 * i + c));
+            
+        }
 
-            }
-            for (int i = 0; i < 3; i++)
+        private void PaintCube(PaintEventArgs e)
+        {
+            var c = 200/Math.Cos(0.523599);
+            int x = 200/3;
+            var y = c / 3;
+            for (float i = 0; i < 3; i++)
             {
-                g.DrawLine(Pens.Black, 300 - 200 / 3 * i, (float)(50 + c / 6 * i), 500 - 200 / 3 * i, (float)(165 + c / 6 * i));
-                g.DrawLine(Pens.Black, 300 + 200 / 3 * i, (float)(50 + c / 6 * i), 100 + 200 / 3 * i, (float)(165 + c / 6 * i));
+                for (float l = 0; l < 3; l++)
+                {
+                    // Side y
+                    Point[] cellPoints =
+                    {
+                        new Point((int)(300 + l * x), (int)(300 + y * (i - l/2))),
+                        new Point((int)(300 + (l + 1) * x), (int)(300 + i*y - (l+1)/2*y)),
+                        new Point((int)(300 + (l+1)*x), (int)(300 + (i+1)*y - (l+1)*y/2)),
+                        new Point((int)(300 + l*x), (int)(300 + (i+1)*y - l*y/2))
+                    };
+                    Color cellColor = cells[2, (int)(i + l * 3)];
+                    PaintCell(e, cellPoints, cellColor);
+
+                    // Side x
+                    cellPoints[0] = new Point((int)(300 - (i + 1) * x), (int)(300 + l * y - (i + 1) / 2 * y));
+                    cellPoints[1] = new Point((int)(300 - i * x), (int)(300 + l * y - i * y / 2));
+                    cellPoints[2] = new Point((int)(300 - i * x), (int)(300 + (l + 1) * y - i * y / 2));
+                    cellPoints[3] = new Point((int)(300 - (i + 1) * x), (int)(300 + (l + 1) * y - (i + 1) * y / 2));
+                    cellColor = cells[1, (int)(2 - i + l * 3)];
+                    PaintCell(e, cellPoints, cellColor);
+
+                    // Side z
+                    cellPoints[0] = new Point((int)(300 - (i - l) * x), (int)(300 - (i + l + 2) / 2 * y));
+                    cellPoints[1] = new Point((int)(300 - (i - l - 1) * x), (int)(300 - (i + l + 1) / 2 * y));
+                    cellPoints[2] = new Point((int)(300 - (i - l) * x), (int)(300 - (i + l) / 2 * y));
+                    cellPoints[3] = new Point((int)(300 - (i - l + 1) * x), (int)(300 - (i + l + 1) / 2 * y));
+                    cellColor = cells[4, (int)(2 - i + (2 - l) * 3)];
+                    PaintCell(e, cellPoints, cellColor);
+                }
             }
+        }
+
+        private void PaintCell(PaintEventArgs e, Point[] points, Color color)
+        {
+            // Drawing lines
+            e.Graphics.SmoothingMode = SmoothingMode.AntiAlias;
+            Pen blackPen = new Pen(Color.Black, 1);
+            e.Graphics.DrawPolygon(blackPen, points);
+
+            // Brushing polygon
+            int temp = points[0].X == points[2].X ? 1 : 0;
+            points[0] = new Point(points[0].X + temp, points[0].Y + 1);
+            points[0] = new Point(points[0].X -1, points[0].Y + temp);
+            points[0] = new Point(points[0].X - temp, points[0].Y - 1);
+            points[0] = new Point(points[0].X + 1, points[0].Y - temp);
+
+            FillMode newFillMode = FillMode.Winding;
+
+            // Draw polygon to screen.
+            e.Graphics.FillPolygon(new SolidBrush(color), points, newFillMode);
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
         }
     }
 }
